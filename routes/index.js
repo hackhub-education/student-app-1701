6b2f9388bcceb73dad4093474d262887482d4fe8
Yan Hong <onlyhongyan@gmail.com>
2017-01-21 17:44:17 -0800

    create student managment app with express js

---
+++ routes/index.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/webdxd');
+
+var studentSchema = {
+  firstname: String,
+  lastname: String,
+  gender: String,
+  school: String,
+  age: Number,
+  isEnrolled: Boolean
+};
+
+var Students = mongoose.model('Students', studentSchema, 'students');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'WebDxD', author: 'Yan' });
+});
+
+/* GET students page. */
+router.get('/students', function(req,res,next) {
+  Students.find().exec(function(err, doc) {
+    res.render('students', {title: 'All Students', students: doc});
+  });
+});
+
+/* POST add student. */
+router.post('/students/add', function(req, res, next) {
+  var newStudent = new Students(req.body);
+  newStudent.save(function (err, doc) {
+    res.render('studentDetail', {student: doc});
+  });
+});
+
+/* GET add student form */
+router.get('/students/add', function(req, res, next) {
+  res.render('newStudent', {});
+});
+
+/* GET student detail by id. */
+router.get('/students/:id', function(req,res,next) {
+  Students.findById(req.params.id).exec(function(err, doc) {
+    res.render('studentDetail', {student: doc});
+  });
+});
+
+/* POST find students by name. */
+router.post('/students/search', function(req, res, next) {
+  Students.find({'firstname': req.body.firstname}).exec(function(err, doc) {
+    if (err) {
+      // handle error
+    } else {
+      res.render('students', {title: 'Search Results', students: doc});
+    }
+
+  });
+});
+
+module.exports = router;
